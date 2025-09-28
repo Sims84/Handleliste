305c8ebe030df00e5325eae766b09d42a3e95206
Sims84 <gjermoe@gmail.com>
2025-09-28 15:59:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    open: true
+    open: true,
+    host: true, // Allows external access
+    cors: {
+      origin: [
+        "https://49e1a990-de35-449e-83ef-8211dbfd4327-00-3pidl6cfc3eo3.picard.replit.dev"
+      ]
+    }
   }
 })
